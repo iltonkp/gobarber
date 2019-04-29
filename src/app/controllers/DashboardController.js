c36a3204d08e638bdbf92ff91f096c0f410ccf9e
iltonkp <iltonk.si@gmail.com>
2019-04-29 15:05:30 -0300

    Removendo provider logado da lista de providers

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,8 +1,22 @@
 const { User } = require('../models')
+const { Op } = require('sequelize')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
+    const { user } = req.session
+    const providers = await User.findAll({
+      where: {
+        provider: true,
+        [Op.and]: [
+          {
+            id: {
+              [Op.ne]: user.id
+            }
+          }
+        ]
+      }
+    })
+
     return res.render('dashboard', { providers })
   }
 }
